125f50b2fbb576dbff74a8888bc1ab141d77be62
Roberto Adan Sanchez Celis <56656877+Adan444@users.noreply.github.com>
2021-09-22 20:20:27 -0500

    Initial commit

---
+++ app/src/main/java/mx/com/tesoem/edu/isc/g7s21rascp1/MainActivity.java
@@ -0,0 +1,14 @@
+package mx.com.tesoem.edu.isc.g7s21rascp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
